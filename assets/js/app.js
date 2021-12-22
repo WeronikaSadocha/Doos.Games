fefde49d9f6931f80c2ce682044676b0ec5f7057
WeronikaSadocha <w.sadocha@gmail.com>
2021-12-22 12:27:00 +0100

    new changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,16 +7,13 @@ var app= new Vue({
         cart: JSON.parse(localStorage.getItem("cart")),
         products: boardgames,
     imagePath: './assets/images/',
-    farAwayImagePath: '../../assets/images/',
-    lastClickedId: localStorage.getItem("lastId")    
-   
+    lastClickedId: localStorage.getItem("lastId") ,  
     },
     methods:{
         updateCart(id){
           this.cart.push(id);
           localStorage.setItem("cart", JSON.stringify(this.cart))
          
-          
         },
         deleteFromCart(id){
             this.cart.splice(id, 1)
@@ -29,17 +26,15 @@ var app= new Vue({
         saveLastId(id){
         this.lastClickedId = id;
         localStorage.setItem("lastId",this.lastClickedId)
-        }
+        },
         
     },
     computed:{
         arraySum(){
             let total=0;
             let newString= this.cart.map (i=> this.products[i].price)
-
               for(let j=0; j<newString.length; j++){
-                   total =total + newString[j]
-                   
+                   total =total + newString[j] 
                 }
             return total.toFixed(2)
         }
--- assets/js/cart-component.js
+++ assets/js/cart-component.js
@@ -1,6 +1,10 @@
 // CART COMPONENT
 Vue.component('cart',{
-    template:`<table class="table table-hover table-dark">
+    template:`<div> <div class= "cart-items" :class="{toggle:!isActive}" >
+    <span class=closeButtonBasket @click="isActive=!isActive" > &times;</span>
+    <div class= "BasketTitle">Your Basket</div>
+    <div class="container">
+    <table class="table table-hover table-dark">
     <thead>
     </thead>
     <tbody>
@@ -10,15 +14,43 @@ Vue.component('cart',{
         <td>{{products[product].price}}</td>
         <td class="delete-item" v-on:click=" deleteFromCart(cart.indexOf(product)); deleteFromTotal(cartPrice.indexOf(product))">&times</td>
       </tr>
-    
     </tbody>
-  </table> `,
+  </table> 
+  <p > The total price is: {{arraySum}}</p>
+  </div>
+    <button class=checkOutButton> Check out</button>
+  </div> 
+
+  <nav class="navbar navbar-expand-lg animate__animated animate__fadeIn ">
+        <button class="navbar-toggler"   data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="material-icons">density_medium</span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <a class="navbar-brand" href="#"><img class="doos" src=assets/images/doos.png alt="loogo doss games"></a>
+
+            <li class="nav-item">
+              <a class="nav-link" href="#">ABOUT US</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">CONTACT</a>
+            </li>
+          </ul>
+        </div>
+          <div class= "icons">
+                <svg @click="isActive=!isActive" class= "basket" xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-basket3-fill" viewBox="0 0 16 16">
+                      <path d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H.5a.5.5 0 0 1-.5-.5v-1A.5.5 0 0 1 .5 6h1.717L5.07 1.243a.5.5 0 0 1 .686-.172zM2.468 15.426.943 9h14.114l-1.525 6.426a.75.75 0 0 1-.729.574H3.197a.75.75 0 0 1-.73-.574z"/>
+                </svg>
+          </div>
+      </nav>
+      </div>`,
 
   data(){
       return {
         products: boardgames,
     imagePath: './assets/images/',
     cartPrice:[],
+    isActive:false,
  }
  
   },
@@ -42,6 +74,10 @@ Vue.component('cart',{
     this.$emit('delete-from-total',id)
 },
 
-
+},
+computed:{
+  arraySum(){
+    this.$emit('array-sum')
+  }
 }})
 
