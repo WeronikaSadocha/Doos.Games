9be6c42018028da5b696f8ec44dc50406fe0bd18
WeronikaSadocha <w.sadocha@gmail.com>
2021-12-29 15:01:02 +0100

    simple responsive navbar without problems

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,7 +9,8 @@ var app= new Vue({
         cart: JSON.parse(localStorage.getItem("cart")),
         products: boardgames,
     imagePath: './assets/images/',
-    lastClickedId: localStorage.getItem("lastId") ,  
+    lastClickedId: localStorage.getItem("lastId"),
+    isActive: false,  
     },
 
 
@@ -34,5 +35,13 @@ var app= new Vue({
         },
         
     },
+    computed:{
+        arraySum(){
+          let total=0;
+          let newString= this.cart.map (i=> this.products[i].price)
+            for(let j=0; j<newString.length; j++){
+                 total =total + newString[j] 
+              }
+          return total.toFixed(2)}}
 
 })
